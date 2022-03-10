72525f90fda4cce3d7a5f1d10d6e754c8d7f5f11
natanael <nathan0002@outlook.es>
2022-03-10 16:46:05 -0500

    Changed the background color and created a new poem

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ const poems = [
     image: "bee.jpeg",
   },
 
+  {
+    author: "Natanael",
+    poem: "Roses are red ",
+    image: "sunflower.jpeg",
+  },
+
   // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
 ];
 
